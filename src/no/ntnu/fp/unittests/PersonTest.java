d8e55a5914abaf8ddbde45989c67a81344cc5147
Nora Tomas <noratomas@hotmail.com>
2018-04-21 08:49:40 +0200

    added more unitt tests

---
+++ src/no/ntnu/fp/unittests/PersonTest.java
@@ -0,0 +1,33 @@
+import no.ntnu.fp.model.Person;
+import no.ntnu.fp.model.Project;
+import org.junit.Assert;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    private Person linda;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        linda = new Person(8, "Linda Tan", "lindatan@gmail.com", "Downing Street",
+                "London", "45qr78");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void set_email_changes_email(){
+        linda.setEmail("lindaNewMail@gmail.com");
+        Assert.assertEquals("lindaNewMail@gmail.com", linda.getEmail() );
+    }
+
+
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
